0727c5bf89701320894af6993a0bcca6bdb91c7b
JMB <jbruel@gmail.com>
2020-11-05 16:19:45 +0100

    working on gradle

---
+++ ChocolateFactory/src/main/java/ChocolateFactory/BouilleurChocolat.java
@@ -0,0 +1,40 @@
+package ChocolateFactory;
+/**
+ * @author bruel (taken from Design Pattren - Head First, O'Reilly, 09/2004)
+ *
+ */
+public class BouilleurChocolat {
+	private boolean vide;
+	private boolean bouilli;
+
+	public BouilleurChocolat() {
+		vide = true;
+		bouilli = false;
+	}
+
+	public void remplir() {
+		if (estVide()) {
+			vide = false;
+			bouilli = false;
+			// remplir le bouilleur du mélange lait/chocolat
+		}
+	}
+
+	public void vider() {
+		if (!estVide() && estBouilli()) {
+			// vider le mélange
+			vide = true;
+		}
+	}
+
+	public void bouillir() {
+		if (!estVide() && !estBouilli()) {
+			// porter le contenu à ébullition
+			bouilli = true;
+		}
+	}
+
+	public boolean estVide() { return vide;}
+
+	public boolean estBouilli() { return bouilli;}
+}
